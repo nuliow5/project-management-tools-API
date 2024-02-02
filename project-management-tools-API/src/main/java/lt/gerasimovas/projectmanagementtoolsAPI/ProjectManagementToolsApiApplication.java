6cc2f4fa1e1b165425f472d05c8dc716a1346cef
Vadimas Gerasimovas <vadimas@baltictransline.lt>
2024-02-02 08:46:39 +0200

    project start

---
+++ project-management-tools-API/src/main/java/lt/gerasimovas/projectmanagementtoolsAPI/ProjectManagementToolsApiApplication.java
@@ -0,0 +1,15 @@
+package lt.gerasimovas.projectmanagementtoolsAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectManagementToolsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectManagementToolsApiApplication.class, args);
+		System.out.println("== Project Management Tools start ==");
+
+	}
+
+}
---
+++ project-management-tools-API/src/test/java/lt/gerasimovas/projectmanagementtoolsAPI/ProjectManagementToolsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.gerasimovas.projectmanagementtoolsAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectManagementToolsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
